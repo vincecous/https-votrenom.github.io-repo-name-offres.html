9dbdd528dc175b7d51eee6ef50e756c84da01960
vincecous <vincecous@gmail.com>
2025-12-06 10:50:29 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = 'offres-tm-v1';
+const urlsToCache = [
+  './offres.html',
+  './manifest.json'
+];
+
+// Installation du service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache ouvert');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activation du service worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Suppression ancien cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interception des requêtes
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Retourne la version en cache si disponible
+        if (response) {
+          return response;
+        }
+        
+        // Sinon, fait la requête réseau
+        return fetch(event.request).then(response => {
+          // Vérifie si la réponse est valide
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          
+          // Clone la réponse pour la mettre en cache
+          const responseToCache = response.clone();
+          
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseToCache);
+          });
+          
+          return response;
+        });
+      })
+  );
+});
